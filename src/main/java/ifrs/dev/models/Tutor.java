884b9ab222824859ad8691c0a5d0139c2e12d081
Kauana Henriques <karoltvbl@gmail.com>
2023-07-08 16:21:41 -0300

    asocia animais a um tutor

--- src/main/java/ifrs/dev/models/Tutor.java
+++ src/main/java/ifrs/dev/models/Tutor.java
@@ -1,5 +1,7 @@
 package ifrs.dev.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,9 +14,10 @@ public class Tutor {
     private Long id;
     private String name;
     private String email;
+    private List<Animal> animals;
 
     public Tutor() {
-        //Construtor vazio
+        // Construtor vazio
     }
 
     public Tutor(String name, String email) {
@@ -29,16 +32,28 @@ public class Tutor {
     public void setId(Long id) {
         this.id = id;
     }
+    
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
+    
     public String getEmail() {
         return email;
     }
+    
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
     }
 }
