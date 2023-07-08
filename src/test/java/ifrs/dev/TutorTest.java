d566ac31bc6d4cead3e23166ee9abc3cf04ff2c9
Kauana Henriques <karoltvbl@gmail.com>
2023-07-08 19:25:53 -0300

    add para os metodos relacionados a animal

--- src/test/java/ifrs/dev/TutorTest.java
+++ src/test/java/ifrs/dev/TutorTest.java
@@ -1,18 +1,25 @@
 package ifrs.dev;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ifrs.dev.models.Animal;
 import ifrs.dev.models.Tutor;
 
 public class TutorTest {
 
     private Tutor tutor;
+    private Animal animal1;
+    private Animal animal2;
 
     @BeforeEach
     public void setup() {
         tutor = new Tutor("Maria", "maria@tst.com");
+        animal1 = new Animal("Luci", "Dog");
+        animal2 = new Animal("Max", "Cat");
     }
 
     @Test
@@ -39,6 +46,34 @@ public class TutorTest {
         tutor.setEmail("joana@tst.com");
         String email = tutor.getEmail();
         Assertions.assertEquals("joana@tst.com", email);
+    }
+
+    @Test
+    public void testGetAnimals() {
+        tutor.addAnimal(animal1);
+        tutor.addAnimal(animal2);
+
+        List<Animal> animals = tutor.getAnimals();
+
+        Assertions.assertNotNull(animals);
+        Assertions.assertEquals(2, animals.size());
+        Assertions.assertTrue(animals.contains(animal1));
+        Assertions.assertTrue(animals.contains(animal2));
+    }
+
+    @Test
+    public void testAddAnimal() {
+        List<Animal> animals = tutor.getAnimals();
+        Assertions.assertNotNull(animals);
+        Assertions.assertEquals(0, animals.size());
+
+        tutor.addAnimal(animal1);
+        Assertions.assertEquals(1, animals.size());
+        Assertions.assertTrue(animals.contains(animal1));
+
+        tutor.addAnimal(animal2);
+        Assertions.assertEquals(2, animals.size());
+        Assertions.assertTrue(animals.contains(animal2));
     }
 
     @Test
