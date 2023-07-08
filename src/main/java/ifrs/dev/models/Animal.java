4108abba318fa883f0d0017be9037e34758b1496
Kauana Henriques <karoltvbl@gmail.com>
2023-07-08 12:25:52 -0300

    add model para animal

---
+++ src/main/java/ifrs/dev/models/Animal.java
@@ -0,0 +1,48 @@
+package ifrs.dev.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String breed;
+    
+    public Animal() {
+    }
+    
+    @ManyToOne
+    private Tutor tutor;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getBreed() {
+        return breed;
+    }
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+    public Tutor getTutor() {
+        return tutor;
+    }
+    public void setTutor(Tutor tutor) {
+        this.tutor = tutor;
+    }
+}
+
