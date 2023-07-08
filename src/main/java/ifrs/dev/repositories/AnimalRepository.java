c8229652253956cb47e254ed27627796d6e94490
Kauana Henriques <karoltvbl@gmail.com>
2023-07-08 20:28:08 -0300

    correção sonar

--- src/main/java/ifrs/dev/repositories/AnimalRepository.java
+++ src/main/java/ifrs/dev/repositories/AnimalRepository.java
@@ -8,10 +8,12 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 @ApplicationScoped
 public class AnimalRepository implements PanacheRepository<Animal> {
 
+    @Override
     public void persist(Animal animal) {
         persistAndFlush(animal);
     }
 
+    @Override
     public Animal findById(Long id) {
         return findById(id);
     }
--- src/main/java/ifrs/dev/repositories/TutorRepository.java
+++ src/main/java/ifrs/dev/repositories/TutorRepository.java
@@ -8,10 +8,12 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 @ApplicationScoped
 public class TutorRepository implements PanacheRepository<Tutor> {
 
+    @Override
     public void persist(Tutor tutor) {
         persistAndFlush(tutor);
     }
 
+    @Override
     public Tutor findById(Long id) {
         return findById(id);
     }
